108e8f7c199a7db142cb492ad28a3643b2668a8e
flasky06 <bonnienjuguna106@gmail.com>
2024-02-02 11:44:57 +0300

    create api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    "connected to mongodb";
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3700, () => {
+      console.log("Server running on port 3700 !");
+    });
+  })
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-app.listen(4500, () => {
-  console.log("Server running on port 4500 !");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res
+    .status(statusCode)
+    .json({ success: false, error: message, statusCode });
 });
+
+app.use("/api/user", userRoutes);
