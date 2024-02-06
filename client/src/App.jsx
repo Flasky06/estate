0053463c683a7b72254b3ad3726fc8fc279d815e
flasky06 <bonnienjuguna106@gmail.com>
2024-02-06 22:44:58 +0300

    update header component & add protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-slate-200 font-bold">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -9,15 +12,25 @@ function Header() {
           <h1>Auth App</h1>
         </Link>
         <ul className="flex gap-4">
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/about">
-            <li>About</li>
-          </Link>
-          <Link to="/signin">
-            <li>Sign in</li>
-          </Link>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              {currentUser ? (
+                <img
+                  src={currentUser.profilePicture}
+                  alt="profile"
+                  className="w-7 h-7 rounded-full object-cover "
+                />
+              ) : (
+                <li>Sign In</li>
+              )}
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
